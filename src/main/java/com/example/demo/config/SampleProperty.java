0d1e0d11d2dd1afd9caf25cc68cd367a9f35974b
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2022-12-17 00:04:43 +0900

    Create prpperty

--- src/main/java/com/example/demo/config/SampleProperty.java
+++ src/main/java/com/example/demo/config/SampleProperty.java
@@ -1,4 +1,16 @@
 package com.example.demo.config;
 
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:property/sample.property")
+@Getter
+@ToString
 public class SampleProperty {
+    @Value("${csv.path}")
+    private String csvPath;
 }
